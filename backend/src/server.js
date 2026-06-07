106a2cd1b971ee78f60c98419ee4206787c7cd0e
ayushluck <luckyayush2005@gmail.com>
2026-06-07 10:34:19 +0530

    DB and Tools Setup done successfully

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,29 @@
 import express from 'express';
-import { ENV } from './lib/env.js';
+import path from 'path';
+import { connectDB } from '../lib/db.js';
+import { ENV } from '../lib/env.js';
 const app = express();
 
-app.listen(ENV.PORT, (err)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log("Server is running on port " + ENV.PORT);
-})
+const __dirname = path.resolve();
+
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("/{*anything}", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+
+const startServer = async () => {
+    try{
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+            console.log(`Server is running on port ${ENV.PORT}`);
+        });
+    }catch(error){
+        console.error("Failed to start server:", error);
+    } 
+};
+
+startServer();
+
