92c991513a84fb89820cfc9a9f7e72f68048e3ec
ayushluck <luckyayush2005@gmail.com>
2026-06-18 21:43:46 +0530

    implemented session route and callerid

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import { serve } from "inngest/express";
 import { inngest, functions } from '../lib/inngest.js';
 import { clerkMiddleware } from "@clerk/express";
 import chatRoutes from '../routes/chatRoutes.js';
+import sessionRoutes from '../routes/sessionRoutes.js';
 const app = express();
 
 const __dirname = path.resolve();
@@ -15,6 +16,8 @@ app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(clerkMiddleware());
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
+
 if (ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("/*", (req, res) => {
